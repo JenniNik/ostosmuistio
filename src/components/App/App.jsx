3b18abe4c844006d0c4a2ce7e79e68522b181eda
Jenni Nikander <jenni.nikander@edu.sasky.fi>
2024-05-12 22:03:01 +0300

    lisää Content-komponentin

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import Header from '../Header'
 import Item from '../Item'
 import styles from './App.module.scss'
 import Menu from '../Menu'
+import Content from '../Content'
 
 
 function App() {
@@ -10,14 +11,17 @@ function App() {
         <>
           <div className={styles.app}>
             <Header />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
+            <Content>
+              <Item />
+              <Item />
+              <Item />
+              <Item />
+            </Content>
             <Menu />
           </div>
         </>
       )
+    
     
     
     
