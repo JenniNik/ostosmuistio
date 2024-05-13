c2a4b3f8e8a67a670c4a315f8661b6670274d6ef
Jenni Nikander <jenni.nikander@edu.sasky.fi>
2024-05-13 18:32:35 +0300

    lisää ButtonContainer-komponentin

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,28 +4,30 @@ import styles from './App.module.scss'
 import Menu from '../Menu'
 import Content from '../Content'
 import { FloatingButton } from '../../shared/buttons'
+import { ButtonContainer } from '../../shared/buttons'
 
 
 
 function App() {
 
-    return (
-        <>
-          <div className={styles.app}>
-            <Header />
-            <Content>
-              <Item />
-              <Item />
-              <Item />
-              <Item />             
-              
-              <FloatingButton primary>+</FloatingButton>
-
-            </Content>
-            <Menu />
-          </div>
-        </>
-      )
+  return (
+    <>
+      <ButtonContainer>
+      <div className={styles.app}>
+        <Header />
+        <Content>
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+          <FloatingButton primary>+</FloatingButton>
+        </Content>
+        <Menu />
+      </div>
+      </ButtonContainer>
+    </>
+  )
+   
     
     
     
