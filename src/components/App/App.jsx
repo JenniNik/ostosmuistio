f26bda42b876bbc6f46ac19306dee6c2bd6a604f
Jenni Nikander <jenni.nikander@edu.sasky.fi>
2024-05-14 20:33:40 +0300

    lisää ympyräkaavioon värityksen

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -47,7 +47,7 @@ function App() {
                  onItemSubmit={handleItemSubmit}
                  onItemDelete={handleItemDelete}
                  onTypeSubmit={handleTypeSubmit} />
-```'
+
 
 
 
