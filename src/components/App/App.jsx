804d334fd9ecaa60a57673ae802c13363068c7f2
Jenni Nikander <jenni.nikander@edu.sasky.fi>
2024-05-15 17:04:53 +0300

    lisää kulutyyppien lisäämisen Firestore-tietokantaan

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,21 @@
-import AppRouter from '../AppRouter'
 import { useState } from 'react'
-import useLocalStorage from '../../shared/uselocalstorage'
+import useLocalStorage from '../../shared/uselocalstorage/uselocalstorage'
+import AppRouter from '../AppRouter'
+import testdata from './testdata.js'
 import firebase from './firebase.js'
-import { collection, deleteDoc, doc, getFirestore, onSnapshot, orderBy, query, setDoc  } from 'firebase/firestore'
-
+import { addDoc, collection, deleteDoc, doc, getFirestore, onSnapshot, orderBy, query, setDoc  } from 'firebase/firestore'
 import { useEffect } from 'react'
 
-
-
 function App() {
+
   const [data, setData] = useState([])
-  const [typelist, setTypelist] = useLocalStorage('ostosmuistio-typelist',[])
+  
+  const [typelist, setTypelist] = useState([])
 
   const firestore = getFirestore(firebase)
 
   useEffect( () => {
+    
     const unsubscribe = onSnapshot(query(collection(firestore,'item'),
                                          orderBy('paymentDate', 'desc')),
                                    snapshot => {
@@ -27,41 +28,39 @@ function App() {
     return unsubscribe
   }, [])
 
-
+  useEffect( () => {
+    const unsubscribe = onSnapshot(query(collection(firestore,'type'),
+                                         orderBy('type')),
+                                   snapshot => {
+      const newTypelist = []
+      snapshot.forEach( doc => {
+        newTypelist.push(doc.data().type)
+      })
+      setTypelist(newTypelist)
+    })
+    return unsubscribe
+  }, [])
 
   const handleItemDelete = async (id) => {
     await deleteDoc(doc(firestore, 'item', id))
   }
-
-
   const handleItemSubmit = async (newitem) => {
     await setDoc(doc(firestore, 'item', newitem.id), newitem)
   }
 
-  const handleTypeSubmit = (type) => {
-    let copy = typelist.slice()
-    copy.push(type)
-    copy.sort()
-    setTypelist(copy)
+  
+  const handleTypeSubmit = async (type) => {
+    await addDoc(collection(firestore,'type'),{type: type})
   }
-
 
   return (
     <>
-                      <AppRouter data={data}
+      <AppRouter data={data}
                  typelist={typelist}
                  onItemSubmit={handleItemSubmit}
                  onItemDelete={handleItemDelete}
                  onTypeSubmit={handleTypeSubmit} />
-
-
-
-
-  
-
-
     </>
   )
 }
-
 export default App
