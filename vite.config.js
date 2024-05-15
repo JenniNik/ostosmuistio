37838b9fe96ed66fa9e80af6d291c1c01200ae50
Jenni Nikander <jenni.nikander@edu.sasky.fi>
2024-05-15 19:26:14 +0300

    lisää PWA-lisäosan ja sen määritykset

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa"
+import manifest from './manifest.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
