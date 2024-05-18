3f4500bf688089034e78a0b0de8884f7aeca09a5
Jenni Nikander <jenni.nikander@edu.sasky.fi>
2024-05-18 23:04:40 +0300

    Uusi

--- src/components/AddItem/AddItem.jsx
+++ src/components/AddItem/AddItem.jsx
@@ -5,6 +5,7 @@ function AppItem(props) {
 
   return (
     <div>
+      <h1>Ostosmuistio</h1>
       <div className={styles.additem}></div>
       <h2>Uusi ostos</h2>
       <ItemForm onItemSubmit={props.onItemSubmit} typelist={props.typelist} />
